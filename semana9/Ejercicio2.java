fb1b733e43a8c19f596ef1dc1ed4a42a753f7d1b
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2018-05-31 10:39:44 -0500

    Semana9

---
+++ semana9/Ejercicio2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana9;
+
+/**
+ *
+ * @author majimbo1
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int[] arreglo = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i]%2==0);
+                System.out.printf("Tabla%d\n", arreglo[i]);
+                for (int j = 1; j <= 12; j++){
+                    int mult;
+                    mult = arreglo [i] * j;
+                    System.out.printf("%d*%d=%d\n", arreglo [i], j, mult);
+                }
+            }
+        }
+    }
